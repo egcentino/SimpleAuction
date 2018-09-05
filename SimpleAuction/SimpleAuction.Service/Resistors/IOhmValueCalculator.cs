c118a76f80e2879db0d183010ad3911e6b92f9a8
Dwine <egcentino@yahoo.com>
2018-09-05 07:14:42 -0400

    Resistance Calculation. MVC
    Simple auction showcase. Basic HTML. Will apply cosmetics later. Also will try it on Forms

---
+++ SimpleAuction/SimpleAuction.Service/Resistors/IOhmValueCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAuction.Service.Resistors
+{
+    public interface IOhmValueCalculator
+    {
+        /// <summary> 
+        /// Calculates the Ohm value of a resistor based on the band colors. /// </summary>
+        /// <param name="bandAColor">The color of the first figure of component value band.</param>
+        /// <param name="bandBColor">The color of the second significant figure band.</param>
+        /// <param name="bandCColor">The color of the decimal multiplier band.</param>
+        /// <param name="bandDColor">The color of the tolerance value band.</param> 
+        int CalculateOhmValue(string bandAColor, string bandBColor, string bandCColor, string bandDColor);
+    }
+
+    public interface IOhmValueCalculator2 : IOhmValueCalculator
+    {
+        double CalculateOhmValue2(string bandAColor, string bandBColor, string bandCColor, string bandDColor);
+    }
+}
---
+++ SimpleAuction/SimpleAuction.Web.Mvc/Scripts/Application/Resistor/Index.js
@@ -0,0 +1,14 @@
+﻿
+SimpleAuction.Resistor = {};
+SimpleAuction.Resistor.Index = (function () {
+
+    function init() {
+    //alert('basePath= ' + SimpleAuction.basePath());
+    //alert('controllerPath= ' + SimpleAuction.controllerPath());
+}
+    return {
+        init: init,
+    }
+}());
+
+SimpleAuction.Resistor.Index.init();
